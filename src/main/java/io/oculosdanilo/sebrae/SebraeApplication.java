df7b291ef27cd8bb07edaab0a9a5092f9fcecfa7
oculosdanilo <oculosdanilo@gmail.com>
2024-07-29 20:30:40 -0300

    primeiro commit

---
+++ src/main/java/io/oculosdanilo/sebrae/SebraeApplication.java
@@ -0,0 +1,13 @@
+package io.oculosdanilo.sebrae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebraeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SebraeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/oculosdanilo/sebrae/SebraeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.oculosdanilo.sebrae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SebraeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
