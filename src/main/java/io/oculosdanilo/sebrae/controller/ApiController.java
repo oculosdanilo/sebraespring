f2c1ad390e3a9f55fa5962f009bfd3d2e2efa0a8
oculosdanilo <oculosdanilo@gmail.com>
2024-08-05 22:21:30 -0300

    morri

--- src/main/java/io/oculosdanilo/sebrae/controller/ApiController.java
+++ src/main/java/io/oculosdanilo/sebrae/controller/ApiController.java
@@ -1,14 +1,24 @@
 package io.oculosdanilo.sebrae.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.oculosdanilo.sebrae.dto.Usuario;
 
 @RestController
 public class ApiController {
 
 	@GetMapping("api")
 	public String executarApi() {
-		return "olá mundinho limdo §;-P)<br />api executada com sucexo";
+		return "olá mundinho limdo §;-P)<br />api executada com sucesso";
+	}
+
+	@PostMapping("cadastro-usuario")
+	public ResponseEntity<?> cadastrarUsuario(@RequestBody Usuario user) {
+		System.out.println(user.toString());
+		return ResponseEntity.ok("cadastrou, beijos");
 	}
 }
---
+++ src/main/java/io/oculosdanilo/sebrae/dto/Usuario.java
@@ -0,0 +1,38 @@
+package io.oculosdanilo.sebrae.dto;
+
+public class Usuario {
+	private String nome;
+	private String email;
+	private String senha;
+	private String telefone;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	@Override
+	public String toString() {
+		return "Usuario [nome=" + nome + ", email=" + email + ", senha=" + senha + ", telefone=" + telefone + "]";
+	}
+}
